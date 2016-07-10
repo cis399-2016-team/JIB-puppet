723587c78987aab447695f3fa4fabae72dba7876
John Brodnax <Brodnaxxx@Johns-MacBook-Pro.local>
2016-07-10 16:48:51 -0700

    Removed ssh class components from init.pp

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,16 +1,8 @@
 class ssh {
-	include ssh::install, ssh::config, ssh::service
-}
-
-class ssh::install {
 
 	package { "ssh":
 		ensure => present,
 	}
-
-}
-
-class ssh::config {
 
 	file { "/etc/ssh/sshd_config":
 		ensure => present,
@@ -21,17 +13,10 @@ class ssh::config {
 		notify => Class["ssh::service"],
 	}
 
-}
-
-class ssh::service {
-
 	service { "ssh":
 		ensure => running,
 		hasstatus => true,
 		hasrestart => true,
 		enable => true,
 	}
-
 }
-
-Class["ssh::install"] -> Class["ssh::config"] -> Class["ssh::service"]
