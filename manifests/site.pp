97db0f4d3f3da103e48ae020a2bfb2c7e35313a0
Brenda Heng <brenda_heng@yahoo.com>
2016-07-11 11:54:19 -0700

    added public key for 3rd node

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node ip-10-0-2-226 {
 		ensure => 'present',
 		user   => 'ec2-user',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCPd+QMa3RsWNIwlmh1ZjpO18dJqVeIFUXhqXqZYBANgECByAlOBoUMCTTN8a27EBOHzThaiUsIGuBXf+iNYwRsFnf4T0QG6Uc/S43xwnLhq5A+1O77l7kiQ3/EMAJqarCP5JfN2pXeMAW4pTQYHfpxZrjtolmnb280AqFFdQeebO4Cxr1t6XiGUZQ0txieevoCRsBHNwqorbA0zfoponFKSU9k+woLnMhNx52+KOKWppmpRr0+NXa6NijxROTC08ptzCGXRGNVOnBu8kI7fVHPNz70t4AutHFFqtJ4GGtt/UqeMS8yyJ2A2dinF2Ow/gyhLf+4m6Mr4Ok5vqpFuSVv',
 	}
 }
 
