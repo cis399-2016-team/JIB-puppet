0c26c7b663a12369630f5df1dd4c817926dca259
John Brodnax <Brodnaxxx@Johns-MacBook-Pro.local>
2016-07-10 17:37:40 -0700

    Added ssh_authorized_keys to manifests/site.pp for node 10-0-3-181

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,14 @@ import '/etc/puppet/modules/sshd/manifests/init.pp'
 
 node ip-10-0-3-181 {
 	include ssh
+
+	ssh_authorized_key { 'JIB-jbrodnax2-laptop':
+		ensure => 'present',
+		user => 'ec2-user',
+		type => 'ssh-rsa',
+		key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCPd+QMa3RsWNIwlmh1ZjpO18dJqVeIFUXhqXqZYBANgECByAlOBoUMCTTN8a27EBOHzThaiUsIGuBXf+iNYwRsFnf4T0QG6Uc/S43xwnLhq5A+1O77l7kiQ3/EMAJqarCP5JfN2pXeMAW4pTQYHfpxZrjtolmnb280AqFFdQeebO4Cxr1t6XiGUZQ0txieevoCRsBHNwqorbA0zfoponFKSU9k+woLnMhNx52+KOKWppmpRr0+NXa6NijxROTC08ptzCGXRGNVOnBu8kI7fVHPNz70t4AutHFFqtJ4GGtt/UqeMS8yyJ2A2dinF2Ow/gyhLf+4m6Mr4Ok5vqpFuSVv',
+	}
+
 }
 
 node ip-10-0-2-53 {
