80335f3b50eade12d84292f1b805fd76f6350b8c
John Brodnax <Brodnaxxx@Johns-MacBook-Pro.local>
2016-07-17 16:41:18 -0700

    Added git pull cron job to master node

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,13 @@
 node ip-10-0-2-153 {
 	include users
 	include ssh
+
+	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+
 }
 
 node ip-10-0-3-181 {
