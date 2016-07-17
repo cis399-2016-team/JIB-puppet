35dee6bdebeb2b4bc9a08d9e4c93ecbf17901436
John Brodnax <Brodnaxxx@Johns-MacBook-Pro.local>
2016-07-17 15:46:19 -0700

    Cleaned up ssh class and fileserver.conf

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,39 +1,3 @@
-#class ssh {
-#	include ssh::install, ssh::config, ssh::service
-#}
-#
-#class ssh::install {
-#
-#	package { "ssh":
-#		ensure => present,
-#	}
-#}
-#
-#class ssh::config {
-#
-#	file { "/etc/ssh/sshd_config":
-#		ensure => present,
-#		mode => 644,
-#		source => "puppet:///modules/ssh/sshd_config",
-#		notify => Class["ssh::service"],
-#	}
-#
-#}
-#
-#class ssh::service {
-#
-#	service { "ssh":
-#		ensure => running,
-#		hasstatus => true,
-#		hasrestart => true,
-#		enable => true,
-#		#subscribe => File['/etc/ssh/sshd_config'],
-#	}
-#
-#}
-#
-#Class["ssh::install"] -> Class["ssh::config"] -> Class["ssh::service"]
-
 class ssh {
 
 	package { "openssh-server":
