13ef45f9c8e772aea0af158df6fa07a77a67369c
John Brodnax <Brodnaxxx@128-223-223-193-wireless.uoregon.edu>
2016-07-21 14:53:23 -0700

    Removed extra jib-jbrodnax ssh key

--- modules/ec2-agents/manifests/init.pp
+++ modules/ec2-agents/manifests/init.pp
@@ -14,13 +14,6 @@ class ec2-agents {
                 key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCPd+QMa3RsWNIwlmh1ZjpO18dJqVeIFUXhqXqZYBANgECByAlOBoUMCTTN8a27EBOHzThaiUsIGuBXf+iNYwRsFnf4T0QG6Uc/S43xwnLhq5A+1O77l7kiQ3/EMAJqarCP5JfN2pXeMAW4pTQYHfpxZrjtolmnb280AqFFdQeebO4Cxr1t6XiGUZQ0txieevoCRsBHNwqorbA0zfoponFKSU9k+woLnMhNx52+KOKWppmpRr0+NXa6NijxROTC08ptzCGXRGNVOnBu8kI7fVHPNz70t4AutHFFqtJ4GGtt/UqeMS8yyJ2A2dinF2Ow/gyhLf+4m6Mr4Ok5vqpFuSVv',
         }
 
-	#ssh_authorized_key { 'JIB-jbrodnax2-laptop':
-        #        ensure => 'present',
-         #       user => 'JIB',
-          #      type => 'ssh-rsa',
-           #     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCPd+QMa3RsWNIwlmh1ZjpO18dJqVeIFUXhqXqZYBANgECByAlOBoUMCTTN8a27EBOHzThaiUsIGuBXf+iNYwRsFnf4T0QG6Uc/S43xwnLhq5A+1O77l7kiQ3/EMAJqarCP5JfN2pXeMAW4pTQYHfpxZrjtolmnb280AqFFdQeebO4Cxr1t6XiGUZQ0txieevoCRsBHNwqorbA0zfoponFKSU9k+woLnMhNx52+KOKWppmpRr0+NXa6NijxROTC08ptzCGXRGNVOnBu8kI7fVHPNz70t4AutHFFqtJ4GGtt/UqeMS8yyJ2A2dinF2Ow/gyhLf+4m6Mr4Ok5vqpFuSVv',
-        #}
-
 	ssh_authorized_key { 'bheng-key-pair-oregon':
             ensure => 'present',
             user   => 'ec2-user',
