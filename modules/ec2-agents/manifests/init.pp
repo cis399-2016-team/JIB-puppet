07b74e04d9be4f9df3ce1308a02471c7af17b0f3
Brenda Heng <brenda_heng@yahoo.com>
2016-07-21 18:56:35 -0700

    added orange-banana key pairs

--- modules/ec2-agents/manifests/init.pp
+++ modules/ec2-agents/manifests/init.pp
@@ -20,6 +20,19 @@ class ec2-agents {
             type   => 'ssh-rsa',
             key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZZ3Gd+3oE71KdZBBP+5xkHizev7Jr5hCGC8mrn3Wef0Aql/hpGGDrAH0hEjeGs+nYMkyFM2EJH+Nt+hMXg59Pssruvgrv+5pVjIiDPucwxgcWHfyhwy7hUlReDiwz7C9VLZXgHdyv8vbMeSDPCO0sNXD1LLL6f6WjHMqsBXi8PXtwe9Bp+ZG+rsz1Odn2IxsYXC0d6kALfknVRDXQxXDFS1HN3vtti5uRMhUEt8/0ceqUDaLfXJ/IbnMtc1wzfV2VAcacvCzlbdjp1bO1Y6ToE4l9UvL9+5bp4mtEZHo61q913M4s2N2/HMk7IvYNJnlFKKAN6wEMlHnkBM7a36hX',
         }
+#Orange-Banana
+
+	ssh_authorized_key { "CIS399":
+		user => "orange-banana",,
+		type => "ssh-rsa",
+		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCx/EgedQlVAGuFLUNtrJfGYIB4ieO1YcrD3ssoOtVA97YgaLE5VA6J0aazUevfNzsbGhrocGM3QZnStrZdAAhd3fyx+SrPXxVldxwpX70jNJg0KoOUEL7hYpSI8bAqzXSkJhCrxrfZMfhrSPQcS4SyBn3DOTc9xce7D93QW7uO0feluY9BzXiScBgOdCHqlTvZroV4Me1pXZ6mPRWnOqwUMwBTFhcNDKbhsftSYmMtjWuucqjpWTHrRMxO4OGfJk/+IQjPgOxwMG8fZnhZ//OE7YL0thDN0hJ6Ko9I5azztepyOleU5lNgTAqGqRrayhspAby0/VM6/UKy7n+1sjdh",
+	}
+
+	ssh_authorized_key { "jstuemke-key-pair-oregon":
+		user => "orange-banana",,
+		type => "ssh-rsa",
+		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCPnelozzko4JcJxfgbm0Ib7h8C0dDpV5NvuKKuzVabFRSlc8MTXUz2aLAaiZzqMQQFdnxOLz0xJrQIpQIGRFiTvBfP1g5fCcHTRcjumv8e5qL+ARGbYgoCz+CMTsF71ZT4Y5U0nJaBuzgKiCNtXjG2Hma/pMT7dEWeUZiOymHihVzUkMa8kAiLkzRJB94ZCv8QeYyO7+cJsjbce4FwBQg+r9+Rwoj11eVj4fOvA1eOhp2d+S31E4GaaEfYrM3lRKQZQLLdygKxiHA3geHMJAJLQaiF3scfm/5NofTS6OxRsoC3q54Q6JXH104okrPYBt/EMRMEY3bO8FI364+LDU4J",
+	}
 
 #Chilled-Beer
 
