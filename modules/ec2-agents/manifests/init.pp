65b676069d052632fc2f3272c2fc40d369bb5404
Brenda Heng <brenda_heng@yahoo.com>
2016-07-25 12:52:27 -0700

    change in costello-key-pair

--- modules/ec2-agents/manifests/init.pp
+++ modules/ec2-agents/manifests/init.pp
@@ -91,7 +91,7 @@ class ec2-agents {
         user => "caluyacostello",
 	type => "ssh-rsa",
         key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCPrg48+84u65bZqmcD9yRJd2ILijuiZY7+iFVgQtXi3HH2s0Pc807oo20lD9fVvjzMFlAUluAQ8GGaLAzTY3bN2iGDzvhtCrI0O9r6u/60WKmYmryuVXunTz28IiISH04AQhNvS3B4mAXE5ZlOtBONao15bQO9ulnH1/LWjtOhh/+XeF4TjyvctM5z+hU5BzGmH7zcPm3Uz9+bnq9yVqSBJXt9HSVba0TEzuQv8MIkZnXJmaSdVtyOY7MVqNo91UCD0IfBqvapt7iUXApU0Zkwoi/ybojz2bcjrf8EPE8GJ4cI6wQ957GuqoD7P2G7vJuQpF3A0fDtzGIbHvn33SKZ",
-        }
+ }
 
         ssh_authorized_key { "elijahcaluya_key_pair":
         user => "caluyacostello",
