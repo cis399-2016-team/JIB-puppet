712204db4ba82ebf7d68719191e6630aedca776d
John Brodnax <Brodnaxxx@128-223-223-192-wireless.uoregon.edu>
2016-07-27 15:09:20 -0700

    Modified sshd module with a case for operating system type

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,27 +1,59 @@
 class sshd {
 
-	package { "openssh-server":
+        package {
+                "openssh-server": ensure => installed;
+        }
 
-		ensure => present,
+	case $operatingsystem {
+		'Ubuntu':{
+			$key_user = "ubuntu"
+			$service_name = "ssh"
+			}
+		'Amazon':{
+			$key_user = "ec2-user"
+			$service_name = "sshd"
+			}
 	}
+
+	#package { "openssh-server":
+	#
+	#	ensure => present,
+	#}
+
+	#file { "/etc/ssh/sshd_config":
+	#	ensure => present,
+	#	owner => 'root',
+	#	group => 'root',
+	#	mode => '644',
+	#	source => "puppet:///modules/sshd/sshd_config",
+	#	notify => Service['sshd'],
+	#}
 
 	file { "/etc/ssh/sshd_config":
-		ensure => present,
-		owner => 'root',
-		group => 'root',
-		mode => '644',
-		source => "puppet:///modules/sshd/sshd_config",
-		notify => Service['sshd'],
+                source => ["puppet:///modules/sshd/$hostname/sshd_config",
+			  "puppet:///modules/sshd/sshd_config",],
+                mode => 644,
+                owner => root,
+                group => root,
+		require => Package["openssh-server"],
 	}
 
-	service { "sshd":
-		ensure => running,
-		hasstatus => true,
-		hasrestart => true,
-		enable => true,
-		require => Package["openssh-server"],
-		subscribe => File["/etc/ssh/sshd_config"],
+	service {$service_name:
+                enable => true,
+                ensure => running,
+                require => [Package["openssh-server"],
+                        File["/etc/ssh/sshd_config"],],
+                subscribe => File["/etc/ssh/sshd_config"],
 	}
+
+	#service { "sshd":
+	#	ensure => running,
+	#	hasstatus => true,
+	#	hasrestart => true,
+	#	enable => true,
+	#	require => Package["openssh-server"],
+	#	subscribe => File["/etc/ssh/sshd_config"],
+	#}
 
 	#service { "ssh":
         #        ensure => running,
