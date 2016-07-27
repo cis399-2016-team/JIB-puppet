e954e65c47eec71dba7d8fa167cdb1758d853774
John Brodnax <Brodnaxxx@128-223-223-192-wireless.uoregon.edu>
2016-07-27 15:19:50 -0700

    Cleaned up sshd module

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -15,20 +15,6 @@ class sshd {
 			}
 	}
 
-	#package { "openssh-server":
-	#
-	#	ensure => present,
-	#}
-
-	#file { "/etc/ssh/sshd_config":
-	#	ensure => present,
-	#	owner => 'root',
-	#	group => 'root',
-	#	mode => '644',
-	#	source => "puppet:///modules/sshd/sshd_config",
-	#	notify => Service['sshd'],
-	#}
-
 	file { "/etc/ssh/sshd_config":
                 source => ["puppet:///modules/sshd/$hostname/sshd_config",
 			  "puppet:///modules/sshd/sshd_config",],
@@ -36,34 +22,16 @@ class sshd {
                 owner => root,
                 group => root,
 		require => Package["openssh-server"],
-		notify => [$service_name],
 	}
 
 	service {$service_name:
                 enable => true,
                 ensure => running,
+		hasstatus => true,
+		hasrestart => true,
                 require => [Package["openssh-server"],
                         File["/etc/ssh/sshd_config"],],
                 subscribe => File["/etc/ssh/sshd_config"],
 	}
-
-	#service { "sshd":
-	#	ensure => running,
-	#	hasstatus => true,
-	#	hasrestart => true,
-	#	enable => true,
-	#	require => Package["openssh-server"],
-	#	subscribe => File["/etc/ssh/sshd_config"],
-	#}
-
-	#service { "ssh":
-        #        ensure => running,
-        #        hasstatus => true,
-        #        hasrestart => true,
-        #        enable => true,
-        #        require => Package["openssh-server"],
-                #subscribe => File["/etc/ssh/sshd_config" ],
-        #}
-
 
 }
